c953f0e4cff331945492aaa1db40f6c7b2343f5f
Christoph Bühler <buehler@users.noreply.github.com>
2023-04-14 11:20:18 +0200

    feat(credentials): attach response content to service account auth if it fails (#483)

--- tests/Zitadel.Test/Credentials/ServiceAccount.Test.cs
+++ tests/Zitadel.Test/Credentials/ServiceAccount.Test.cs
@@ -21,4 +21,13 @@ public class ServiceAccountTest
 
         token.Should().NotBeEmpty();
     }
+    
+    [Fact]
+    public async Task Should_Throw_With_Meaningful_Error()
+    {
+        var sa = await ServiceAccount.LoadFromJsonStringAsync(TestData.InvalidServiceAccountJson);
+        var ex = await Assert.ThrowsAsync<HttpRequestException>(() => sa.AuthenticateAsync(TestData.ApiUrl));
+
+        ex.Message.Should().Contain("invalid signature");
+    }
 }
