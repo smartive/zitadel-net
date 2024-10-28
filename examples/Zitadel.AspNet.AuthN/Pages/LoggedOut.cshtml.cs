496e11b5eb3b8996d3692e455a2bcd70bb07fc47
Joshua Nitschke <33307206+JoshuaNitschke@users.noreply.github.com>
2024-10-28 00:32:51 -0700

    chore: allow proper logout in examples (#929)
    Co-authored-by: Christoph Bühler <buehler@users.noreply.github.com>

---
+++ examples/Zitadel.AspNet.AuthN/Pages/LoggedOut.cshtml.cs
@@ -0,0 +1,7 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Zitadel.AspNet.AuthN.Pages;
+
+public class LoggedOutModel : PageModel { }
